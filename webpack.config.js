e2d3a791628eeb50c7d7c3ca5607c68c28a453af
Will Scott <will@cypherpunk.email>
2021-02-01 11:47:00 -0800

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'demo'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './demo',
+  }
 };
