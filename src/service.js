de49d4dd2e7b8d94259793dc2fc846df9c48f5a7
Will Scott <will@cypherpunk.email>
2021-02-04 10:05:37 -0800

    provide additional bootstrap

--- src/service.js
+++ src/service.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const ipfs = require('ipfs');
+//const filters = require('libp2p-websockets/src/filters')
+//const Websockets = require('libp2p-websockets')
 
 let node = null;
+//const transportKey = Websockets.prototype[Symbol.toStringTag]
 
 module.exports = async () => {
     if (!node) {
@@ -18,10 +21,17 @@ module.exports = async () => {
                     },
                     pubsub: {
                         enabled: true
-                    }
+                    },
+                    //transport: {
+                    //    [transportKey]: {
+                    //      filter: filters.all
+                    //    }
+                    //  }
                 }
             }
         })
+        await (await node).swarm.connect("/dns4/nxdomain.dev/tcp/443/wss/p2p/QmVLS4EgCdw8onfFLU9t25a4oAbGvetRhD9jyN5UziDQiu")
+        //await (await node).swarm.connect("/ip4/127.0.0.1/tcp/4002/ws/p2p/QmVLS4EgCdw8onfFLU9t25a4oAbGvetRhD9jyN5UziDQiu")
     }
     return await node;
 }
